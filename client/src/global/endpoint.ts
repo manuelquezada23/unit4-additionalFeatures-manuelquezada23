aab15c2b319f3b1aa55bbb81e949fd6360a7e438
Manuel Quezada <manuel_quezada@brown.edu>
2023-11-17 00:55:30 -0500

    adding prod endpoint

--- client/src/global/endpoint.ts
+++ client/src/global/endpoint.ts
@@ -1,5 +1,5 @@
 const devEndpoint = "http://localhost:8000/";
-const prodEndpoint = "https://cs1951v-editablenodes.onrender.com/";
+const prodEndpoint = "https://cs1951v-additionalfeatures.onrender.com/";
 
 const isProduction = process.env.NODE_ENV === "production";
 export const endpoint = isProduction ? prodEndpoint : devEndpoint;
